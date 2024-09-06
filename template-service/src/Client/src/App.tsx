38c16db4b1506b24db22d44617d5313577c6b9c1
Maksim Shauchenka <Maksim.Shauchenka@ventionteams.com>
2024-09-06 14:21:03 +0200

    [FE] add shadcn

--- template-service/src/Client/src/App.tsx
+++ template-service/src/Client/src/App.tsx
@@ -1,34 +1,19 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+
+import { Button } from "@/components/ui/button";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="flex items-center justify-center min-h-screen flex-col items-center">
+      <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0 mb-2">
+        Vite + React + TS
+      </h2>
+      <Button onClick={() => setCount((count) => count + 1)}>
+        count is {count}
+      </Button>
+    </div>
   );
 }
 
--- template-service/src/Client/vite.config.ts
+++ template-service/src/Client/vite.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,11 +9,16 @@ export default defineConfig({
   preview: {
     port: 8080,
     strictPort: true,
-   },
-   server: {
+  },
+  server: {
     port: 8080,
     strictPort: true,
     host: true,
     origin: "http://0.0.0.0:8080",
-   },
-})
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
